99eb6d952a58c19f17e1a6529021a850b34e9f09
Vinicius-Barbosa-Santos <viniciusfoxsantos@gmail.com>
2024-07-23 16:07:21 -0300

    feat: forum with ddd & clean architecture - test factories

--- src/domain/forum/application/use-cases/get-question-by-slug.spec.ts
+++ src/domain/forum/application/use-cases/get-question-by-slug.spec.ts
@@ -1,33 +1,29 @@
-import { Question } from '../../../../domain/forum/enterprise/entities/question'
-import { GetQuestionBySlugUseCase } from './get-question-by-slug'
-import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-repository'
-import { Slug } from '../../../../domain/forum/enterprise/entities/value-objects/slug'
-import { UniqueEntityID } from '../../../../core/entities/unique-entity-id'
+import { GetQuestionBySlugUseCase } from "./get-question-by-slug";
+import { InMemoryQuestionsRepository } from "test/repositories/in-memory-questions-repository";
+import { Slug } from "../../../../domain/forum/enterprise/entities/value-objects/slug";
+import { makeQuestion } from "test/factories/make-question";
 
-let inMemoryQuestionsRepository: InMemoryQuestionsRepository
-let sut: GetQuestionBySlugUseCase
+let inMemoryQuestionsRepository: InMemoryQuestionsRepository;
+let sut: GetQuestionBySlugUseCase;
 
-describe('Get Question By Slug', () => {
+describe("Get Question By Slug", () => {
   beforeEach(() => {
-    inMemoryQuestionsRepository = new InMemoryQuestionsRepository()
-    sut = new GetQuestionBySlugUseCase(inMemoryQuestionsRepository)
-  })
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository();
+    sut = new GetQuestionBySlugUseCase(inMemoryQuestionsRepository);
+  });
 
-  it('should be able to get a question by slug', async () => {
-    const newQuestion = Question.create({
-      authorId: new UniqueEntityID(),
-      title: 'Example Question',
-      slug: Slug.create('example-question'),
-      content: 'Example content',
-    })
+  it("should be able to get a question by slug", async () => {
+    const newQuestion = makeQuestion({
+      slug: Slug.create("example-question"),
+    });
 
-    await inMemoryQuestionsRepository.create(newQuestion)
+    await inMemoryQuestionsRepository.create(newQuestion);
 
     const { question } = await sut.execute({
-      slug: 'example-question',
-    })
+      slug: "example-question",
+    });
 
-    expect(question.id).toBeTruthy()
-    expect(question.title).toEqual(newQuestion.title)
-  })
-})
+    expect(question.id).toBeTruthy();
+    expect(question.title).toEqual(newQuestion.title);
+  });
+});
