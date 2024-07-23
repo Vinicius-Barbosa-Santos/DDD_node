bdd6df93102af79208d87b5604968d993a4a2946
Vinicius-Barbosa-Santos <viniciusfoxsantos@gmail.com>
2024-07-23 13:34:07 -0300

    feat: forum with ddd & clean architecture - refactoring unit tests

--- src/domain/forum/application/use-cases/answer-question.ts
+++ src/domain/forum/application/use-cases/answer-question.ts
@@ -3,9 +3,13 @@ import { Answer } from "../../enterprise/entities/answer";
 import { AnswersRepository } from "../repositories/answers-repository";
 
 interface AnswerQuestionUseCaseRequest {
-  instructorId: string;
-  questionId: string;
-  content: string;
+  instructorId: string
+  questionId: string
+  content: string
+}
+
+interface AnswerQuestionUseCaseResponse {
+  answer: Answer
 }
 
 export class AnswerQuestionUseCase {
@@ -15,15 +19,17 @@ export class AnswerQuestionUseCase {
     instructorId,
     questionId,
     content,
-  }: AnswerQuestionUseCaseRequest) {
+  }: AnswerQuestionUseCaseRequest): Promise<AnswerQuestionUseCaseResponse> {
     const answer = Answer.create({
       content,
       authorId: new UniqueEntityID(instructorId),
       questionId: new UniqueEntityID(questionId),
-    });
+    })
 
-    await this.answersRepository.create(answer);
+    await this.answersRepository.create(answer)
 
-    return answer;
+    return {
+      answer,
+    }
   }
 }
